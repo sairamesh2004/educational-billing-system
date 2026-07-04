69448de0c1e0a5c5c4b67490ddb3847c757f877d
Sai Ramesh Vajjala <145277796+sairamesh2004@users.noreply.github.com>
2026-07-04 09:15:52 +0530

    Add main class with entry point for application

---
+++ src/main.java
@@ -0,0 +1,15 @@
+
+public class MAIN {
+
+	public static void main(String[] args) {
+
+		Enterypage ep = new Enterypage();
+		ep.setTitle(" Academy");
+		ep.setSize(1000, 600);
+		ep.setVisible(true);
+
+		
+
+	}
+
+}
